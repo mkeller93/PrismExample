62ffa2c5c1dd3cf2e0cad93889de2d962fc5b002
Marcel Keller <marcel.keller1993@gmail.com>
2017-12-04 10:46:30 +0100

    renamed shell project to .Shell

---
+++ PrismExample.Shell/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Unity;
+using PrismExample.Shell.Views;
+
+namespace PrismExample.Shell
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var catalog = (ModuleCatalog)ModuleCatalog;
+            catalog.AddModule(typeof(Modules.Person.PersonModule));
+            catalog.AddModule(typeof(Modules.Car.CarModule));            
+        }
+    }
+}
---
+++ PrismExample.Shell/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace PrismExample.Shell.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Prism Unity Application";
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            
+        }
+    }
+}
